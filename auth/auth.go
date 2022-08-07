616b06c56abf61b930a65268b8d9c0733fe7c09d
xeusteerapat <xeus085@gmail.com>
2022-08-07 16:12:38 +0700

    :lock: [Add] add jwt token

---
+++ auth/auth.go
@@ -0,0 +1,29 @@
+package auth
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v4"
+)
+
+func AccessToken(ctx *gin.Context) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, &jwt.StandardClaims{
+		ExpiresAt: time.Now().Add(5 * time.Minute).Unix(), // valid for 5 mins (unix timestamp)
+	})
+
+	signedString, err := token.SignedString([]byte("==signature=="))
+
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+		})
+
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{
+		"token": signedString,
+	})
+}
