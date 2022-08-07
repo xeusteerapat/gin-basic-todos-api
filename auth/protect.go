59bc500a68d25fec6b4a91a70a49c5034f66bb29
xeusteerapat <xeus085@gmail.com>
2022-08-07 16:45:14 +0700

    :boom: [Update] update to middleware func

--- auth/protect.go
+++ auth/protect.go
@@ -2,18 +2,31 @@ package auth
 
 import (
 	"fmt"
+	"net/http"
+	"strings"
 
+	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt/v4"
 )
 
-func Protect(tokenString string) error {
-	_, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+func Protect(signature []byte) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		authToken := ctx.Request.Header.Get("Authorization")
+		tokenString := strings.TrimPrefix(authToken, "Bearer ")
+
+		_, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+			}
+
+			return signature, nil
+		})
+
+		if err != nil {
+			ctx.AbortWithStatus(http.StatusUnauthorized)
+			return
 		}
 
-		return []byte("==signature=="), nil
-	})
-
-	return err
+		ctx.Next()
+	}
 }
